4ed7290f3abf599d39ec498209aae20054b24150
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-02-29 13:54:49 +0200

    build: use bitnikcode/bento4 docker tag

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ BENTO4_VERSION=1.6.0-641
 
 docker build \
   --build-arg "BENTO4_VERSION=$BENTO4_VERSION" \
-  -t "bento4:$BENTO4_VERSION" \
-  -t bento4:latest \
+  -t "bitnikcode/bento4:$BENTO4_VERSION" \
+  -t bitnikcode/bento4:latest \
   .
