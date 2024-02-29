92db454b956ff270b37d81d872052e9eaa4c6696
Niko Korhonen <1004138+niktheblak@users.noreply.github.com>
2024-02-29 15:57:32 +0200

    build: use niktheblak as org

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ BENTO4_VERSION=1.6.0-641
 
 docker build \
   --build-arg "BENTO4_VERSION=$BENTO4_VERSION" \
-  -t "bitnikcode/bento4:$BENTO4_VERSION" \
-  -t bitnikcode/bento4:latest \
+  -t "niktheblak/bento4:$BENTO4_VERSION" \
+  -t niktheblak/bento4:latest \
   .
